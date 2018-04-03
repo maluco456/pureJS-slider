4fa1797b684d82779886988fd5e0943a8463e7fc
maluco456 <alexandreartedesigner@gmail.com>
2018-04-03 11:34:45 -0300

    NAvigations
    Almost done, just a few more steps

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
+var ul = document.getElementById("carrousel"),
+    ulLength = ul.children.length,
+    index =1;
+
 function createSlider(id){
         slider= document.getElementById(id),
         positionInfo = slider.getBoundingClientRect(),
         sliderHeight = positionInfo.height,
         sliderWidth = positionInfo.width,
-        sliderContainerWidth = parseInt(document.getElementById(id).offsetWidth),
+        sliderContainerWidth = document.getElementById(id).offsetWidth,
         ul = document.getElementById("carrousel"),
         ulLength = ul.children.length,
-        n=1,
+        n=0,
         dots = [],
         dotsNumber = ulLength,
         i = 0,
@@ -18,26 +22,25 @@ function createSlider(id){
     renderDots =  function(n){
         var span = document.createElement('span');
         span.setAttribute("class","dot");
-        span.setAttribute("onclick","currentSlide('" + n + "')");
+        span.setAttribute("onclick","currentSlide('carrousel','" + n + "')");
         div.appendChild(span);
     };
 
     next.setAttribute("class","next");
     next.setAttribute("id","next_"+id);
-    //next.setAttribute("onclick","plusSlides(1)");
+    next.setAttribute("onclick","transitionSlides('carrousel',1)");
     next.innerHTML = "&#10095;";
     slider.appendChild(next);
-    //next.addEventListener("click", teste() );
 
     prev.setAttribute("class","prev");
     prev.setAttribute("id","prev_"+id);
-    //prev.setAttribute("onclick","plusSlides(-1)");
+    prev.setAttribute("onclick","transitionSlides('carrousel',-1)");
     prev.innerHTML = "&#10094;";
     slider.appendChild(prev);
 
     div.setAttribute("class","dots-holder");
     slider.appendChild(div);
-    for( n; n<=dotsNumber; n++){
+    for( n; n<dotsNumber; n++){
         renderDots(n);
         dots[n] += 1;
     }
@@ -54,7 +57,28 @@ function createSlider(id){
     setUlWidth();
     setLiWidth();
 }
+var index = 0,
+    n=1;
+transitionSlides = function(id,n){
+    var slideWidth = document.getElementById(id).children[0].style.width,
+        left = document.getElementById(id).style.left,
+        ul = document.getElementById("carrousel"),
+        ulLength = ul.children.length-1,
+        ulWidth = ul.children[0].offsetWidth;
+    index = index + (n);
+    if( index > ulLength ){ index=0; }
+    if ( index < 0){ index = ulLength; }
+    document.getElementById(id).style.left = "-" + (ulWidth*index) + "px" ;
+    console.log(index);
+}
+currentSlide = function(id,n){
+    ulWidth = document.getElementById('carrousel').children[0].offsetWidth;
+    console.log(n);
+    console.log(ulWidth);
+    document.getElementById(id).style.left = '-' + ulWidth * n + 'px';
+}
+
 createSlider("slider");
 
-var ul = document.getElementById("carrousel"),
-    ulLength = ul.children.length;
+
+
